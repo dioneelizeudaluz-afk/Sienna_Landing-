87eb879cbf31c9c3acb5a3e3a2f6ca5aaea11c02
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-09-17 14:28:06 +0200

    SIENNA landing page premium completa

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+export const CONFIG = {
+  SITE_NAME: 'Sienna',
+  
+  CHECKOUT_URL: 'https://checkout.escalepay.com/4052897',
+  
+  PREVIEW_GROUP_URL: 'https://t.me/+YLPqOdmxvbgyYzY0',
+  
+  MAIN_TELEGRAM_URL: 'https://t.me/+FuoS4pIVDuAyM2M8',
+  
+  EBOOK_URL: 'COLOCAR_LINK_DO_EBOOK_AQUI',
+  
+  TELEGRAM_DOWNLOAD_URL: 'https://telegram.org/dl',
+  
+  ADMIN_PASSWORD: 'sienna2024',
+};
+
+export type Language = 'pt' | 'en' | 'es';
