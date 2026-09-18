19cfcf60b0f133fedecf80b035a6fb4c758837e9
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-09-18 23:18:55 +0200

    Remover botao VIP e deixar so grupo gratis

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
 export const CONFIG = {
   SITE_NAME: 'Sienna',
-  CHECKOUT_URL: 'https://checkout.escalepay.com/4052897',
   PREVIEW_GROUP_URL: 'https://t.me/+YLPqOdmxvbgyYzY0',
   BACKGROUND_VIDEO: '/Tay Usagi (100)-pmtdo.mp4',
   COVER_IMAGE: '/sienna-cover.jpg',
